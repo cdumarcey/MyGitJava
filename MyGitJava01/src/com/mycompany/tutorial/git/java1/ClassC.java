51a2f8f6d923ba77c34916173b00100d70e15a93
Cedric Dumarcey <cedric.dumarcey@gmail.com>
2013-12-13 13:13:28 +0100

    Add ClassC

---
+++ MyGitJava01/src/com/mycompany/tutorial/git/java1/ClassC.java
@@ -0,0 +1,17 @@
+package com.mycompany.tutorial.git.java1;
+
+public class ClassC {
+	
+	private String name;
+	
+	
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
